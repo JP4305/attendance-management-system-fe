d8f50d0ad1d4f1fa25ba2f477cf974b084888843
jaylimbachiya43 <109168679+jaylimbachiya43@users.noreply.github.com>
2023-10-11 12:25:03 +0530

    new code

--- next.config.js
+++ next.config.js
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        config.module.rules.push({
-          test: /\.svg$/,
-          issuer: {
-            test: /\.(js|ts)x?$/,
-           // for webpack 5 use
-           // { and: [/\.(js|ts)x?$/] }
-          },
-          
-          use: ['@svgr/webpack'],
-        });
-    
-        return config;
-      },
 }
 
 module.exports = nextConfig
