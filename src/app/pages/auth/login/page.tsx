a311e9d5756bf8ebe8854299a16722adc162368f
Jay Parmar <190770107115@socet.edu.in>
2023-10-13 19:36:24 +0530

    login page(half)

--- src/app/pages/auth/login/page.tsx
+++ src/app/pages/auth/login/page.tsx
@@ -1,10 +1,12 @@
-import LoginPage from '@/components/LoginPage/index'
+import LoginPage from '@/sections/LoginPage/index';
+import LoginPage1 from '@/sections/LoginPage1/index';
 
 export default function Login() {
 
   return (
     <div>
-      <LoginPage/>
+      {/* <LoginPage/> */}
+      <LoginPage1/>
     </div>
   );
 }
